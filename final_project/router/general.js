cea6733d70bbb00994093bd5aa9417f063b44fab
Miguel Arango <miguel@email.com>
2026-01-06 14:53:57 -0500

    Tarea 2

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -17,8 +17,10 @@ public_users.get('/',function (req, res) {
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
+ const isbn = req.params.isbn;
+ const bookIsbn = books[isbn] || null;
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  return res.send(bookIsbn);
  });
   
 // Get book details based on author
